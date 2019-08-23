778ad2092f95f5b2764ad6681348dfdb1053ddfe
Michal Rentka <michalrentka@gmail.com>
2019-08-23 11:34:27 +0200

    items store wip

---
+++ Zotero/Views/ItemsView.swift
@@ -0,0 +1,23 @@
+//
+//  ItemsView.swift
+//  Zotero
+//
+//  Created by Michal Rentka on 23/08/2019.
+//  Copyright © 2019 Corporation for Digital Scholarship. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemsView: View {
+    @ObservedObject private(set) var store: CollectionsStore
+
+    var body: some View {
+        
+    }
+}
+
+struct ItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemsView()
+    }
+}
