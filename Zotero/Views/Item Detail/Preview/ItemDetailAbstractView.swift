817819691b4f45069954078b787ee46b39c77a13
Michal Rentka <michalrentka@gmail.com>
2019-10-03 18:42:24 +0200

    item detail abstract row layout changes

--- Zotero/Views/Item Detail/Preview/ItemDetailAbstractView.swift
+++ Zotero/Views/Item Detail/Preview/ItemDetailAbstractView.swift
@@ -14,7 +14,9 @@ struct ItemDetailAbstractView: View {
     var body: some View {
         VStack(alignment: .leading) {
             ItemDetailMetadataTitleView(title: "Abstract")
+                .padding(.vertical, 6)
             Text(self.abstract)
+                .padding(.bottom)
         }
     }
 }
@@ -23,7 +25,9 @@ struct ItemDetailAbstractView: View {
 
 struct ItemDetailAbstractView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemDetailAbstractView(abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.")
+        List {
+            ItemDetailAbstractView(abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.")
+        }
     }
 }
 
