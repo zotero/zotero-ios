140f0b594fe3ed4936c737674f4b333228f06c94
Michal Rentka <michalrentka@gmail.com>
2019-10-30 16:57:36 +0100

    Removed "attachment" from type picker when editing item

--- Zotero/Controllers/Stores/ItemTypePickerStore.swift
+++ Zotero/Controllers/Stores/ItemTypePickerStore.swift
@@ -14,7 +14,8 @@ class ItemTypePickerStore: ObservableObject, TypePickerStore {
 
     init(selected: String , schemaController: SchemaController) {
         let types: [TypePickerData] = schemaController.itemTypes.compactMap { type in
-            guard let name = schemaController.localized(itemType: type) else { return nil }
+            guard type != ItemTypes.attachment,
+                  let name = schemaController.localized(itemType: type) else { return nil }
             return TypePickerData(key: type, value: name)
         }.sorted(by: { $0.value < $1.value })
         self.state = TypePickerState(data: types, selectedRow: selected)
