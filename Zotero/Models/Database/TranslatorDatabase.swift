88fc32e1f06ffe93d356fd60f8bc412353558cb4
Michal Rentka <michalrentka@gmail.com>
2020-04-16 11:08:10 +0200

    Fixed translator error, migrated db

--- Zotero/Models/Database/TranslatorDatabase.swift
+++ Zotero/Models/Database/TranslatorDatabase.swift
@@ -11,7 +11,7 @@ import Foundation
 import RealmSwift
 
 struct TranslatorDatabase {
-    private static let schemaVersion: UInt64 = 0
+    private static let schemaVersion: UInt64 = 1
 
     static var configuration: Realm.Configuration {
         return Realm.Configuration(fileURL: Files.translatorsDbFile.createUrl(),
