d362273108778d184bf85bb4ba56a9afefaa945c
Michal Rentka <michalrentka@gmail.com>
2020-07-23 15:43:44 +0200

    #25 - finished login screen implementation, iPhone fixes, textfield interaction added

---
+++ Zotero/Scenes/General/ViewModels/KeyboardResponder.swift
@@ -0,0 +1,9 @@
+//
+//  KeyboardResponder.swift
+//  Zotero
+//
+//  Created by Michal Rentka on 23/07/2020.
+//  Copyright © 2020 Corporation for Digital Scholarship. All rights reserved.
+//
+
+import Foundation
---
+++ Zotero/Scenes/General/Views/SelectableTextField.swift
@@ -0,0 +1,21 @@
+//
+//  SelectableTextField.swift
+//  Zotero
+//
+//  Created by Michal Rentka on 23/07/2020.
+//  Copyright © 2020 Corporation for Digital Scholarship. All rights reserved.
+//
+
+import SwiftUI
+
+struct SelectableTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectableTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectableTextField()
+    }
+}
