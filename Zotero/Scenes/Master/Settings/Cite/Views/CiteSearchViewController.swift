00930df5eefc8e8de234f008d6c8ee2e71f7c11f
Michal Rentka <michalrentka@gmail.com>
2022-06-14 12:00:29 +0200

    Removed custom diffable data source, removed differ dependency, updated other dependencies

--- Zotero/Scenes/Master/Settings/Cite/Views/CiteSearchViewController.swift
+++ Zotero/Scenes/Master/Settings/Cite/Views/CiteSearchViewController.swift
@@ -18,7 +18,7 @@ class CiteSearchViewController: UIViewController {
     private let pickAction: (RemoteStyle) -> Void
     private let disposeBag: DisposeBag
 
-    private var dataSource: DiffableDataSource<Int, RemoteStyle>!
+    private var dataSource: UITableViewDiffableDataSource<Int, RemoteStyle>!
     weak var coordinatorDelegate: CitationStyleSearchSettingsCoordinatorDelegate?
 
     init(viewModel: ViewModel<CiteSearchActionHandler>, pickAction: @escaping (RemoteStyle) -> Void) {
@@ -69,11 +69,11 @@ class CiteSearchViewController: UIViewController {
 
         if state.changes.contains(.styles) {
             let styles = state.filtered ?? state.styles
-            var snapshot = DiffableDataSourceSnapshot<Int, RemoteStyle>(isEditing: false)
-            snapshot.append(section: 0)
-            snapshot.append(objects: styles, for: 0)
-            let animation: DiffableDataSourceAnimation = state.changes.contains(.loading) ? .none : .rows(reload: .automatic, insert: .automatic, delete: .automatic)
-            self.dataSource.apply(snapshot: snapshot, animation: animation, completion: nil)
+
+            var snapshot = NSDiffableDataSourceSnapshot<Int, RemoteStyle>()
+            snapshot.appendSections([0])
+            snapshot.appendItems(styles, toSection: 0)
+            self.dataSource.apply(snapshot)
         }
     }
 
@@ -82,11 +82,12 @@ class CiteSearchViewController: UIViewController {
     private func setupTableView() {
         self.tableView.delegate = self
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        self.dataSource = DiffableDataSource(tableView: self.tableView, dequeueAction: { tableView, indexPath, _, _ in
-            return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        }, setupAction: { cell, _, _, style in
+
+        self.dataSource = UITableViewDiffableDataSource(tableView: self.tableView, cellProvider: { tableView, indexPath, style in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
             cell.textLabel?.text = style.title
             cell.detailTextLabel?.text = Formatter.sqlFormat.string(from: style.updated)
+            return cell
         })
     }
 
@@ -107,7 +108,7 @@ extension CiteSearchViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
 
-        if let style = self.dataSource.snapshot.object(at: indexPath) {
+        if let style = self.dataSource.itemIdentifier(for: indexPath) {
             self.pickAction(style)
             self.navigationController?.popViewController(animated: true)
         }
