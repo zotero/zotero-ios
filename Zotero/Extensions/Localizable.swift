7dc3c6a3fe94230a46b7b1d47784f702499526a7
Michal Rentka <michalrentka@gmail.com>
2022-09-08 11:45:41 +0200

    Xcode 14 updates

--- Zotero/Extensions/Localizable.swift
+++ Zotero/Extensions/Localizable.swift
@@ -982,7 +982,7 @@ internal enum L10n {
       internal static func message(_ p1: Any) -> String {
         return L10n.tr("Localizable", "settings.crash_alert.message", String(describing: p1))
       }
-      /// Your Report ID is %@. If Zotero crashes repeatedly please tap "Export Database" and send exported files to support@zotero.org
+      /// Your Report ID is %@. If Zotero crashes repeatedly, please tap "Export Database" and send exported files to support@zotero.org.
       internal static func messageWithDb(_ p1: Any) -> String {
         return L10n.tr("Localizable", "settings.crash_alert.message_with_db", String(describing: p1))
       }
