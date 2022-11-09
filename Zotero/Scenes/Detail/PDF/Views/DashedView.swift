457f86012a60a00150a549cfbf8fb632c6f28f38
Michal Rentka <michalrentka@gmail.com>
2022-11-09 09:58:59 +0100

    Implemented toolbar dragging behaviour, improved animations, added overlays with highlighted zones where the toolbar will end up

---
+++ Zotero/Scenes/Detail/PDF/Views/DashedView.swift
@@ -0,0 +1,55 @@
+//
+//  DashedView.swift
+//  Zotero
+//
+//  Created by Michal Rentka on 07.11.2022.
+//  Copyright © 2022 Corporation for Digital Scholarship. All rights reserved.
+//
+
+import UIKit
+
+class DashedView: UIView {
+    private var dashLayer: CAShapeLayer? {
+        return (self.layer.sublayers ?? []).compactMap({ $0 as? CAShapeLayer }).first
+    }
+
+    var dashColor: UIColor {
+        didSet {
+            guard let layer = self.dashLayer else { return }
+            layer.strokeColor = self.dashColor.cgColor
+        }
+    }
+
+    init(dashColor: UIColor) {
+        self.dashColor = dashColor
+        super.init(frame: CGRect())
+        self.addDashedBorder(color: dashColor)
+    }
+
+    required init?(coder: NSCoder) {
+        self.dashColor = .black
+        super.init(coder: coder)
+        self.addDashedBorder(color: .black)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        guard let layer = self.dashLayer else { return }
+
+        layer.frame = self.bounds
+        layer.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.layer.cornerRadius).cgPath
+    }
+
+    private func addDashedBorder(color: UIColor) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame = self.bounds
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = 3
+        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+        shapeLayer.lineDashPattern = [6,3]
+        shapeLayer.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.layer.cornerRadius).cgPath
+        self.layer.addSublayer(shapeLayer)
+    }
+}
