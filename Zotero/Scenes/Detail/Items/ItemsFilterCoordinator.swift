19e8c86f024026aafd81d080535d2ba266cca6b8
Michal Rentka <michalrentka@gmail.com>
2023-04-07 13:52:49 +0200

    #71 - Included new tag picker in filter popup on iPhone

--- Zotero/Scenes/Detail/Items/ItemsFilterCoordinator.swift
+++ Zotero/Scenes/Detail/Items/ItemsFilterCoordinator.swift
@@ -19,11 +19,13 @@ final class ItemsFilterCoordinator: NSObject, Coordinator {
     private unowned let viewModel: ViewModel<ItemsActionHandler>
     unowned let navigationController: UINavigationController
     private unowned let controllers: Controllers
+    private weak var itemsController: ItemsViewController?
 
-    init(viewModel: ViewModel<ItemsActionHandler>, navigationController: NavigationViewController, controllers: Controllers) {
+    init(viewModel: ViewModel<ItemsActionHandler>, itemsController: ItemsViewController, navigationController: NavigationViewController, controllers: Controllers) {
         self.viewModel = viewModel
         self.navigationController = navigationController
         self.controllers = controllers
+        self.itemsController = itemsController
         self.childCoordinators = []
 
         super.init()
@@ -37,7 +39,15 @@ final class ItemsFilterCoordinator: NSObject, Coordinator {
     func start(animated: Bool) {
         guard let dbStorage = self.controllers.userControllers?.dbStorage else { return }
 
-        let controller = ItemsFilterViewController(viewModel: self.viewModel, dbStorage: dbStorage)
+        let selected = self.viewModel.state.tagsFilter ?? []
+        let state = TagFilterState(selectedTags: selected, showAutomatic: Defaults.shared.tagPickerShowAutomaticTags, displayAll: Defaults.shared.tagPickerDisplayAllTags)
+        let handler = TagFilterActionHandler(dbStorage: dbStorage)
+        let tagController = TagFilterViewController(viewModel: ViewModel(initialState: state, handler: handler))
+        tagController.view.translatesAutoresizingMaskIntoConstraints = false
+        tagController.delegate = self.itemsController
+        self.itemsController?.tagFilterDelegate = tagController
+
+        let controller = ItemsFilterViewController(viewModel: self.viewModel, tagFilterController: tagController)
         controller.coordinatorDelegate = self
         self.navigationController.setViewControllers([controller], animated: animated)
     }
