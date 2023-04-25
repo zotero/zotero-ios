ccedcd13ef1d210e8f7c42712f5b3ca584f7df34
Michal Rentka <michalrentka@gmail.com>
2023-04-25 14:03:19 +0200

    #670 - limit sort index y position

--- Zotero/Controllers/AnnotationConverter.swift
+++ Zotero/Controllers/AnnotationConverter.swift
@@ -45,11 +45,14 @@ struct AnnotationConverter {
 
         let textOffset = boundingBoxConverter?.textOffset(rect: rect, page: annotation.pageIndex) ?? 0
         let minY = boundingBoxConverter?.sortIndexMinY(rect: rect, page: annotation.pageIndex).flatMap({ Int(round($0)) }) ?? 0
+        if minY < 0 {
+            DDLogWarn("AnnotationConverter: annotation \(annotation.key) has negative y position \(minY)")
+        }
         return self.sortIndex(pageIndex: annotation.pageIndex, textOffset: textOffset, minY: minY)
     }
 
     static func sortIndex(pageIndex: PageIndex, textOffset: Int, minY: Int) -> String {
-        return String(format: "%05d|%06d|%05d", pageIndex, textOffset, minY)
+        return String(format: "%05d|%06d|%05d", pageIndex, textOffset, max(0, minY))
     }
 
     // MARK: - PSPDFKit -> Zotero
