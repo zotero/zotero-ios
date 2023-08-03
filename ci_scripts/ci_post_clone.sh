fcd6c80a341a1ed744363e43d280374b04e8c3d4
Miltiadis Vasilakis <mvasilak@gmail.com>
2023-08-03 14:53:16 +0300

    Install SwiftGen via CI script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Zotero
+#
+#  Created by Miltiadis Vasilakis on 3/8/23.
+#  Copyright © 2023 Corporation for Digital Scholarship. All rights reserved.
+
+brew install swiftgen
