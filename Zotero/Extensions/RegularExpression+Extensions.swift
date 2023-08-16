3c174ba83bf525ba68af977424a4d5020217e6f0
Michal Rentka <michalrentka@gmail.com>
2023-08-16 15:12:46 +0200

    Updated translator id parsing regex to include only proper translators ids (#749)

--- Zotero/Extensions/RegularExpression+Extensions.swift
+++ Zotero/Extensions/RegularExpression+Extensions.swift
@@ -17,11 +17,27 @@ extension NSTextCheckingResult {
         return Range(self.range(at: index), in: string)
     }
 
+    /// Creates Swift `Range` from result for given group name in string.
+    /// - parameter name: Group name of matched range in this result.
+    /// - parameter string: String from which this result originates.
+    /// - returns: `Range` if index is in bounds, `nil` otherwise.
+    func swiftRange(withName name: String, in string: String) -> Range<String.Index>? {
+        return Range(self.range(withName: name), in: string)
+    }
+
     /// Creates substring from result at given index in string.
     /// - parameter index: Index of matched substring in this result.
     /// - parameter string: String from which this result originates.
     /// - returns: Substring if index is in bounds, `nil` otherwise.
     func substring(at index: Int, in string: String) -> Substring? {
         return self.swiftRange(at: index, in: string).flatMap({ string[$0] })
+    }
+
+    /// Creates substring from result for given group name in string.
+    /// - parameter name: Group name of matched substring in this result.
+    /// - parameter string: String from which this result originates.
+    /// - returns: Substring if index is in bounds, `nil` otherwise.
+    func substring(withName name: String, in string: String) -> Substring? {
+        return self.swiftRange(withName: name, in: string).flatMap({ string[$0] })
     }
 }
