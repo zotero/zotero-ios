d2dac8bcad67e803528f58bd666127cc9db4fa70
Michal Rentka <michalrentka@gmail.com>
2023-09-25 13:07:16 +0200

    Fixed xcode pre-build script to accomodate new pdf-key format

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -7,5 +7,14 @@
 #  Copyright © 2023 Corporation for Digital Scholarship. All rights reserved.
 
 git clone https://github.com/zotero/pdf-keys.git
-mv pdf-keys/licenses.plist licenses/licenses.plist
+license=$(grep -Eo '\"license_key\":\"[A-Za-z0-9_=\-]+\"' pdf-keys/ios_license.json | cut -c 15- | tr -d '"')
+plist='<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>pspdfkit</key>
+	<string>'"$license"'</string>
+</dict>
+</plist>'
+echo "$plist" > licenses/licenses.plist
 rm -rf pdf-keys
