ea1a230e0411f8e1c5bd567aaa2c479619d78fdb
Miltiadis Vasilakis <mvasilak@gmail.com>
2024-01-08 17:21:12 +0200

    Improve DetailCoordinator presented item replacement for multiple items

--- Zotero/Scenes/General/Views/NavigationViewController.swift
+++ Zotero/Scenes/General/Views/NavigationViewController.swift
@@ -13,10 +13,32 @@ class NavigationViewController: UINavigationController {
     var statusBarVisible: Bool = true
 
     override var prefersStatusBarHidden: Bool {
-        return !self.statusBarVisible
+        return !statusBarVisible
     }
 
     deinit {
-        self.dismissHandler?()
+        dismissHandler?()
+    }
+}
+
+class DetailNavigationViewController: NavigationViewController {
+    weak var coordinator: Coordinator?
+    public func replaceContents(with replacement: DetailNavigationViewController, animated: Bool) {
+        // Set replacement properties to self.
+        // Swap coordinators and dismiss handlers, so that the original coordinator is properly deinitialized, along with the original view controllers.
+        // Swap also the navigation controller property of the two coordinators.
+        // Store original
+        let originalCoordinator = coordinator
+        let originalDismissHandler = dismissHandler
+        // Swap replacement to original
+        coordinator = replacement.coordinator
+        coordinator?.navigationController = self
+        dismissHandler = replacement.dismissHandler
+        statusBarVisible = replacement.statusBarVisible
+        setViewControllers(replacement.viewControllers, animated: animated)
+        // Swap original to replacement
+        replacement.coordinator = originalCoordinator
+        replacement.coordinator?.navigationController = replacement
+        replacement.dismissHandler = originalDismissHandler
     }
 }
