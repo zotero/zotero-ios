1a260f730ef2cfb3fb3880f8121977cbdd5112b2
Miltiadis Vasilakis <mvasilak@gmail.com>
2024-02-13 09:53:58 +0200

    Improve annotation comment icon drawing

--- Zotero/Controllers/CommentIconDrawingController.swift
+++ Zotero/Controllers/CommentIconDrawingController.swift
@@ -12,22 +12,24 @@ struct CommentIconDrawingController {
     static let iconSize: CGSize = CGSize(width: 12, height: 12)
 
     static func draw(context: CGContext, boundingBox: CGRect, color: UIColor) {
-        guard let colorizedCgImage = Asset.Images.Annotations.commentColored.image.cgImage,
-              let outlineCgImage = Asset.Images.Annotations.comment.image.cgImage else { return }
+        guard let colorizedCgImage = Asset.Images.Annotations.commentColored.image.cgImage, let outlineCgImage = Asset.Images.Annotations.comment.image.cgImage else { return }
 
-        let size = CommentIconDrawingController.iconSize
+        let size = Self.iconSize
 
         var origin = boundingBox.origin
         origin.y += boundingBox.height - (size.height / 2)
-        origin.x -= size.width
+        origin.x -= (size.width / 2)
 
+        let alpha: CGFloat = 0.4
         let newBoundingBox = CGRect(origin: origin, size: size)
 
         context.clip(to: newBoundingBox, mask: colorizedCgImage)
-        color.setFill()
+        color.withAlphaComponent(alpha).setFill()
         context.fill(newBoundingBox)
 
         context.resetClip()
-        context.draw(outlineCgImage, in: newBoundingBox)
+        context.clip(to: newBoundingBox, mask: outlineCgImage)
+        UIColor.black.withAlphaComponent(alpha).setFill()
+        context.fill(newBoundingBox)
     }
 }
