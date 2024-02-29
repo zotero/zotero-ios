0d341eb1ac217b3cae1d8d16358f6ba2949d5808
Miltiadis Vasilakis <mvasilak@gmail.com>
2024-02-29 09:03:09 +0200

    Suppress PSPDFKitUI.PDFViewController keyCommands

---
+++ Zotero/Extensions/PSPDFKitUI+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  PSPDFKitUI+Extensions.swift
+//  Zotero
+//
+//  Created by Miltiadis Vasilakis on 28/2/24.
+//  Copyright © 2024 Corporation for Digital Scholarship. All rights reserved.
+//
+
+import PSPDFKitUI
+
+extension PSPDFKitUI.PDFViewController {
+    open override var keyCommands: [UIKeyCommand]? {
+        []
+    }
+}
