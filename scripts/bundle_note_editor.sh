4a305cc50186a96048aea40f1914bb65777379fb
Michal Rentka <michalrentka@gmail.com>
2024-03-14 13:51:43 +0100

    Added build script for note editor to xcode

---
+++ scripts/bundle_note_editor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+SCRIPT_PATH=`realpath "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
+EDITOR_DIR="$SCRIPT_DIR/../bundled/note_editor"
+
+if [ -d "$EDITOR_DIR" ]; then
+    rm -rf "$EDITOR_DIR"
+fi
+
+cd "$SCRIPT_DIR/../note-editor"
+npm run build:ios
+mv "$SCRIPT_DIR/../note-editor/build/ios" "$SCRIPT_DIR/../bundled/note_editor"
