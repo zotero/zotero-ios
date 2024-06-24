6d39c4bf96403e3c6bcad2c220251f52a717b601
Miltiadis Vasilakis <mvasilak@gmail.com>
2024-06-24 14:12:30 +0300

    Fix AttachmentDownloader URLSession memory leak

--- Zotero/Controllers/Controllers.swift
+++ Zotero/Controllers/Controllers.swift
@@ -265,7 +265,7 @@ final class Controllers {
         // Disable ongoing sync and unsubscribe from websocket
         controllers?.disableSync(apiKey: self.apiKey)
         // Cancel all downloads
-        controllers?.fileDownloader.cancelAll()
+        controllers?.fileDownloader.cancelAll(invalidateSession: true)
         // Cancel all identifier lookups
         controllers?.identifierLookupController.cancelAllLookups()
         // Cancel all remote downloads
