f55a85fe69910d648d7ad71a585ba6d8d02571dd
Michal Rentka <michalrentka@gmail.com>
2024-07-11 12:59:30 +0200

    wip

---
+++ scripts/bundle_reader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+SCRIPT_PATH=`realpath "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
+SCHEMA_FILE="$SCRIPT_DIR/../Bundled/schema.json"
+
+curl --compressed https://api.zotero.org/schema --output "$SCHEMA_FILE"
