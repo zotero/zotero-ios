6f9d9d34b61a330bbe39d8cdabeee2d6ec4f0f32
Michal Rentka <michalrentka@gmail.com>
2024-07-15 12:31:06 +0200

    build fixes, bug fixes

--- scripts/bundle_reader.sh
+++ scripts/bundle_reader.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
- realpath() {
-     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
- }
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
 
- SCRIPT_PATH=`realpath "$0"`
- SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
- READER_DIR="$SCRIPT_DIR/../bundled/reader"
+SCRIPT_PATH=`realpath "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
+READER_DIR="$SCRIPT_DIR/../bundled/reader"
 
- if [ -d "$READER_DIR" ]; then
-     rm -rf "$READER_DIR"
- fi
+if [ -d "$READER_DIR" ]; then
+ rm -rf "$READER_DIR"
+fi
 
- cd "$SCRIPT_DIR/../reader"
- npm ci
- npm run build:ios
- mv "$SCRIPT_DIR/../reader/build/ios" "$READER_DIR"
+cd "$SCRIPT_DIR/../reader"
+NODE_OPTIONS=--openssl-legacy-provider npm ci
+NODE_OPTIONS=--openssl-legacy-provider npm run build:ios
+mv "$SCRIPT_DIR/../reader/build/ios" "$READER_DIR"
