d8686a459b70d4281037888c5e5149b861059573
Michal Rentka <michalrentka@gmail.com>
2024-07-19 16:12:17 +0200

    WIP: trash controller

---
+++ Zotero/Scenes/Detail/Trash/Models/TrashAction.swift
@@ -0,0 +1,13 @@
+//
+//  TrashAction.swift
+//  Zotero
+//
+//  Created by Michal Rentka on 18.07.2024.
+//  Copyright © 2024 Corporation for Digital Scholarship. All rights reserved.
+//
+
+import Foundation
+
+enum TrashAction {
+    case loadData
+}
