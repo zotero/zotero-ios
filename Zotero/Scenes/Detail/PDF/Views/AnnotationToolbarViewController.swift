d906bd89154607aaee1f2bb382c018473e83e606
Miltiadis Vasilakis <mvasilak@gmail.com>
2024-09-10 16:03:33 +0300

    Fix missing annotation tool from more menu

--- Zotero/Scenes/Detail/PDF/Views/AnnotationToolbarViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/AnnotationToolbarViewController.swift
@@ -387,7 +387,7 @@ class AnnotationToolbarViewController: UIViewController {
         }
 
         if count < toolButtons.count {
-            for idx in count..<toolButtons.count {
+            for idx in (count - 1)..<toolButtons.count {
                 toolButtons[idx] = toolButtons[idx].copy(isHidden: true)
             }
         } else {
