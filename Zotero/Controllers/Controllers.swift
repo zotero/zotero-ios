db954092f86f8cc78a91a84591dfdd9b4eaa7b98
Miltiadis Vasilakis <mvasilak@gmail.com>
2025-02-24 15:09:02 +0200

    Cancel all Recognizer tasks on user logout

--- Zotero/Controllers/Controllers.swift
+++ Zotero/Controllers/Controllers.swift
@@ -262,6 +262,8 @@ final class Controllers {
         userControllers?.remoteFileDownloader.stop()
         // Cancel all background uploads
         userControllers?.backgroundUploadObserver.cancelAllUploads()
+        // Cancel all Recognizer Tasks
+        userControllers?.recognizerController.cancellAllTasks()
         // Cancel all PDF workers
         userControllers?.pdfWorkerController.cancellAllWorks()
         // Clear user controllers
