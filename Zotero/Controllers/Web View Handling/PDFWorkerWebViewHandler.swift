44139c21e73f338f1efce0dd4cdb281f5cbca0d1
Miltiadis Vasilakis <mvasilak@gmail.com>
2025-02-25 18:42:19 +0200

    Refactor PDFWorkerWebViewHandler

--- Zotero/Controllers/Web View Handling/PDFWorkerWebViewHandler.swift
+++ Zotero/Controllers/Web View Handling/PDFWorkerWebViewHandler.swift
@@ -24,6 +24,10 @@ final class PDFWorkerWebViewHandler: WebViewHandler {
         case log = "logHandler"
     }
 
+    enum Error: Swift.Error {
+        case cantFindFile
+    }
+
     enum PDFWorkerData {
         case recognizerData(data: [String: Any])
         case fullText(data: [String: Any])
@@ -45,11 +49,17 @@ final class PDFWorkerWebViewHandler: WebViewHandler {
 
     override func initializeWebView() -> Single<()> {
         DDLogInfo("PDFWorkerWebViewHandler: initialize web view")
-        let baseURL = Bundle.main.bundleURL.appendingPathComponent("Bundled/pdf_worker", isDirectory: true)
-        return loadHTMLString(Self.htmlString, baseURL: baseURL)
+        return loadIndex()
             .flatMap { _ in
                 Single.just(Void())
             }
+
+        func loadIndex() -> Single<()> {
+            guard let indexUrl = Bundle.main.url(forResource: "worker", withExtension: "html") else {
+                return .error(Error.cantFindFile)
+            }
+            return load(fileUrl: indexUrl)
+        }
     }
 
     func recognize(file: FileData) {
@@ -104,87 +114,4 @@ final class PDFWorkerWebViewHandler: WebViewHandler {
             DDLogInfo("JSLOG: \(body)")
         }
     }
-
-    static let htmlString: String = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>PDF Worker</title>
-</head>
-<body>
-<script>
-function log(message) {
-  webkit.messageHandlers.logHandler.postMessage(message);
-}
-
-let promiseId = 0;
-let waitingPromises = {};
-let worker = new Worker('worker.js');
-
-async function query(action, data, transfer) {
-  return new Promise(function (resolve) {
-    promiseId++;
-    waitingPromises[promiseId] = resolve;
-    worker.postMessage({ id: promiseId, action, data }, transfer);
-  });
-}
-
-worker.onmessage = async function (e) {
-  let message = e.data;
-  window.webkit.messageHandlers.logHandler.postMessage('Message received', message);
-  if (message.responseID) {
-    let resolve = waitingPromises[message.responseID];
-    if (resolve) {
-      resolve(message.data);
-    }
-    return;
-  }
-  if (message.id) {
-    window.webkit.messageHandlers.logHandler.postMessage('\thas id: ' + message.id);
-    let respData = null;
-    if (message.op === 'FetchBuiltInCMap') {
-      respData = {
-        compressionType: 1,
-        cMapData: new Uint8Array(await (await fetch('/cmaps/' + message.data + '.bcmap')).arrayBuffer())
-      };
-    }
-    worker.postMessage({ responseID: e.data.id, data: respData });
-    return;
-  }
-}
-
-async function fetchLocalFile(filePath) {
-  log(`fetching ${filePath}`);
-  let response = await fetch(filePath);
-  log(`response: ${JSON.stringify(response)}`);
-  let arrayBuffer = await response.arrayBuffer();
-  return arrayBuffer.slice();
-}
-
-async function recognize(filePath) {
-  try {
-    let buf = await fetchLocalFile(filePath)
-    let recognizerData = await query('getRecognizerData', { buf }, [buf]);
-    webkit.messageHandlers.recognizerDataHandler.postMessage({"recognizerData": recognizerData});
-  } catch (error) {
-    log(`error: ${error}`);
-    throw error;
-  }
-}
-
-async function getFullText(filePath) {
-  try {
-    let buf = await fetchLocalFile(filePath)
-    let fulltext = await query('getFulltext', { buf }, [buf]);
-    webkit.messageHandlers.fullTextHandler.postMessage({"fullText": fulltext});
-  } catch (error) {
-    log(`error: ${error}`);
-    throw error;
-  }
-}
-</script>
-</body>
-</html>
-"""
 }
