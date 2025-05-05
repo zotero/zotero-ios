bfe1daef5149f7f4e809a21fbaf83779dd50a22a
Miltiadis Vasilakis <mvasilak@gmail.com>
2025-05-05 14:58:27 +0300

    Improve code

--- Zotero/Controllers/Files/FileStorage.swift
+++ Zotero/Controllers/Files/FileStorage.swift
@@ -45,7 +45,7 @@ final class FileStorageController: FileStorage {
     }
 
     func write(_ data: Data, to file: File, options: Data.WritingOptions) throws {
-        try self.createDirectories(for: file)
+        try createDirectories(for: file)
         try data.write(to: file.createUrl(), options: options)
     }
 
@@ -53,79 +53,79 @@ final class FileStorageController: FileStorage {
         let url = file.createUrl()
         var isDirectory: ObjCBool = false
 
-        guard self.fileManager.fileExists(atPath: url.path, isDirectory: &isDirectory) else { return }
+        guard fileManager.fileExists(atPath: url.path, isDirectory: &isDirectory) else { return }
 
         if isDirectory.boolValue {
             // This File instance is a directory, remove its contents.
-            let contents = try self.fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: [], options: [])
+            let contents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: [], options: [])
             for url in contents {
-                try self.fileManager.removeItem(at: url)
+                try fileManager.removeItem(at: url)
             }
         }
 
         // Delete the file/directory itself.
-        try self.fileManager.removeItem(at: url)
+        try fileManager.removeItem(at: url)
     }
 
     func copy(from path: String, to toFile: File) throws {
-        try self.createDirectories(for: toFile)
-        try self.fileManager.copyItem(atPath: path, toPath: toFile.createUrl().path)
+        try createDirectories(for: toFile)
+        try fileManager.copyItem(atPath: path, toPath: toFile.createUrl().path)
     }
 
     func copy(from fromFile: File, to toFile: File) throws {
-        try self.createDirectories(for: toFile)
-        try self.fileManager.copyItem(at: fromFile.createUrl(), to: toFile.createUrl())
+        try createDirectories(for: toFile)
+        try fileManager.copyItem(at: fromFile.createUrl(), to: toFile.createUrl())
     }
 
     func move(from fromFile: File, to toFile: File) throws {
-        try self.move(from: fromFile.createUrl().path, to: toFile)
+        try move(from: fromFile.createUrl().path, to: toFile)
     }
 
     func move(from path: String, to toFile: File) throws {
-        try self.createDirectories(for: toFile)
-        try self.fileManager.moveItem(atPath: path, toPath: toFile.createUrl().path)
+        try createDirectories(for: toFile)
+        try fileManager.moveItem(atPath: path, toPath: toFile.createUrl().path)
     }
 
     func has(_ file: File) -> Bool {
-        return self.fileManager.fileExists(atPath: file.createUrl().path)
+        return fileManager.fileExists(atPath: file.createUrl().path)
     }
 
     func size(of path: String) -> UInt64 {
-        let attributes = try? self.fileManager.attributesOfItem(atPath: path)
+        let attributes = try? fileManager.attributesOfItem(atPath: path)
         return (attributes?[FileAttributeKey.size] as? UInt64) ?? 0
     }
 
     func size(of file: File) -> UInt64 {
-        return self.size(of: file.createUrl().path)
+        return size(of: file.createUrl().path)
     }
 
     func createDirectories(for file: File) throws {
         let relativeUrl = file.createRelativeUrl()
-        try self.fileManager.createMissingDirectories(for: relativeUrl)
+        try fileManager.createMissingDirectories(for: relativeUrl)
     }
 
     func sortedContentsOfDirectory(at file: File) throws -> [URL] {
-        return try self.fileManager.contentsOfDirectory(at: file.createUrl(), includingPropertiesForKeys: [.contentModificationDateKey], options: [.skipsHiddenFiles])
-                                   .map({ url in (url, (try? url.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDate ?? Date.distantPast) })
-                                   .sorted(by: { $0.1 < $1.1 })
-                                   .map({ $0.0 })
+        return try fileManager.contentsOfDirectory(at: file.createUrl(), includingPropertiesForKeys: [.contentModificationDateKey], options: [.skipsHiddenFiles])
+            .map({ url in (url, (try? url.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDate ?? Date.distantPast) })
+            .sorted(by: { $0.1 < $1.1 })
+            .map({ $0.0 })
     }
 
     func contentsOfDirectory(at file: File) throws -> [URL] {
-        return try self.fileManager.contentsOfDirectory(at: file.createUrl(), includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsHiddenFiles])
+        return try fileManager.contentsOfDirectory(at: file.createUrl(), includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsHiddenFiles])
     }
 
     func contentsOfDirectory(at file: File) throws -> [File] {
-        return try self.contentsOfDirectory(at: file).map { Files.file(from: $0) }
+        return try contentsOfDirectory(at: file).map { Files.file(from: $0) }
     }
 
     func link(file fromFile: File, to toFile: File) throws {
         let toUrl = toFile.createUrl()
 
-        guard !self.fileManager.fileExists(atPath: toUrl.path) else { return }
+        guard !fileManager.fileExists(atPath: toUrl.path) else { return }
 
-        try self.createDirectories(for: toFile)
-        try self.fileManager.linkItem(at: fromFile.createUrl(), to: toUrl)
+        try createDirectories(for: toFile)
+        try fileManager.linkItem(at: fromFile.createUrl(), to: toUrl)
     }
 
     func directoryData(for files: [File]) -> DirectoryData {
@@ -133,12 +133,30 @@ final class FileStorageController: FileStorage {
         var mbSize: Double = 0
 
         for file in files {
-            guard let fileData = self.directoryData(for: file) else { continue }
+            guard let fileData = directoryData(for: file) else { continue }
             fileCount += fileData.fileCount
             mbSize += fileData.mbSize
         }
 
         return DirectoryData(fileCount: fileCount, mbSize: mbSize)
+
+        func directoryData(for file: File) -> DirectoryData? {
+            let properties: Set<URLResourceKey> = [.totalFileSizeKey, .fileSizeKey, .isRegularFileKey]
+            guard let enumerator = fileManager.enumerator(at: file.createUrl(), includingPropertiesForKeys: Array(properties)) else { return nil }
+
+            var totalSize = 0
+            var count = 0
+
+            for case let url as URL in enumerator {
+                guard let resourceValues = (try? url.resourceValues(forKeys: properties)),
+                      let isRegularFile = resourceValues.isRegularFile, isRegularFile else { continue }
+                let size = resourceValues.totalFileSize ?? resourceValues.fileSize ?? 0
+                totalSize += size
+                count += 1
+            }
+
+            return DirectoryData(fileCount: count, mbSize: (Double(totalSize) / 1048576))
+        }
     }
 
     func isZip(file: File) -> Bool {
@@ -153,24 +171,6 @@ final class FileStorageController: FileStorage {
         defer { handle.closeFile() }
         let data = handle.readData(ofLength: 4)
         return data.starts(with: [0x25, 0x50, 0x44, 0x46])
-    }
-
-    private func directoryData(for file: File) -> DirectoryData? {
-        let properties: Set<URLResourceKey> = [.totalFileSizeKey, .fileSizeKey, .isRegularFileKey]
-        guard let enumerator = self.fileManager.enumerator(at: file.createUrl(), includingPropertiesForKeys: Array(properties)) else { return nil }
-
-        var totalSize = 0
-        var count = 0
-
-        for case let url as URL in enumerator {
-            guard let resourceValues = (try? url.resourceValues(forKeys: properties)),
-                  let isRegularFile = resourceValues.isRegularFile, isRegularFile else { continue }
-            let size = resourceValues.totalFileSize ?? resourceValues.fileSize ?? 0
-            totalSize += size
-            count += 1
-        }
-
-        return DirectoryData(fileCount: count, mbSize: (Double(totalSize) / 1048576))
     }
 
     func isEmptyOrNotFoundResponse(file: File) -> Bool {
