7d898473c5d45dcee0b97e7d52b034b58ad7d14e
Miltiadis Vasilakis <mvasilak@gmail.com>
2025-05-23 11:28:34 +0300

    Enable features during compilation

--- Zotero/Models/FeatureGates.swift
+++ Zotero/Models/FeatureGates.swift
@@ -18,5 +18,32 @@ struct FeatureGates: OptionSet {
     static let htmlEpubReader = FeatureGates(rawValue: 1 << 2)
     static let downloadFilesAtSync = FeatureGates(rawValue: 1 << 3)
 
-    static let enabled: FeatureGates = [.htmlEpubReader, .pdfWorker]
+    static var enabled: FeatureGates {
+        var gates: FeatureGates = []
+
+#if FEATURE_GATE_ALL
+        gates.insert(.multipleOpenItems)
+        gates.insert(.htmlEpubReader)
+        gates.insert(.pdfWorker)
+        gates.insert(.downloadFilesAtSync)
+#else
+#if FEATURE_GATE_HTML_EPUB_READER
+        gates.insert(.htmlEpubReader)
+#endif
+
+#if FEATURE_GATE_PDF_WORKER
+        gates.insert(.pdfWorker)
+#endif
+
+#if FEATURE_GATE_MULTIPLE_OPEN_ITEMS
+        gates.insert(.multipleOpenItems)
+#endif
+
+#if FEATURE_GATE_DOWNLOAD_FILES_AT_SYNC
+        gates.insert(.downloadFilesAtSync)
+#endif
+#endif
+
+        return gates
+    }
 }
