5cefc3cf395aef9febeac98e6b0a007c14cb6dc4
Michal Rentka <michalrentka@gmail.com>
2025-08-13 15:38:29 +0200

    Finished epub search fixes

--- Zotero/Scenes/General/DocumentSearchViewController.swift
+++ Zotero/Scenes/General/DocumentSearchViewController.swift
@@ -30,6 +30,12 @@ struct DocumentSearchResult {
         snippet = pdfResult.previewText
         highlightRange = pdfResult.rangeInPreviewText
     }
+
+    init(snippet: String, highlightRange: NSRange, pageLabel: String? = nil) {
+        self.snippet = snippet
+        self.pageLabel = pageLabel
+        self.highlightRange = highlightRange
+    }
 }
 
 protocol DocumentSearchHandler: AnyObject {
@@ -40,120 +46,6 @@ protocol DocumentSearchHandler: AnyObject {
     func search(for string: String)
     func select(index: Int)
     func cancel()
-}
-
-final class HtmlEpubSearchHandler {
-    private unowned let viewModel: ViewModel<HtmlEpubReaderActionHandler>
-    private unowned let documentController: HtmlEpubDocumentViewController
-    weak var delegate: DocumentSearchDataSourceDelegate?
-
-    init(viewModel: ViewModel<HtmlEpubReaderActionHandler>, documentController: HtmlEpubDocumentViewController) {
-        self.viewModel = viewModel
-        self.documentController = documentController
-    }
-}
-
-extension HtmlEpubSearchHandler: DocumentSearchHandler {
-    var count: Int {
-        // TODO
-        return 0
-    }
-    
-    func result(at index: Int) -> DocumentSearchResult? {
-        // TODO
-        return nil
-    }
-    
-    func search(for string: String) {
-        // TODO
-    }
-    
-    func select(index: Int) {
-        // TODO
-    }
-    
-    func cancel() {
-    }
-}
-
-final class PDFSearchHandler: NSObject {
-    private let document: PSPDFKit.Document
-    private unowned let documentController: PDFDocumentViewController
-
-    private var currentSearch: TextSearch?
-    private var results: [SearchResult]
-    weak var delegate: DocumentSearchDataSourceDelegate?
-
-    init(document: PSPDFKit.Document, documentController: PDFDocumentViewController) {
-        self.document = document
-        self.documentController = documentController
-        results = []
-        super.init()
-    }
-}
-
-extension PDFSearchHandler: DocumentSearchHandler {
-    var count: Int {
-        return results.count
-    }
-
-    func result(at index: Int) -> DocumentSearchResult? {
-        guard index >= 0 && index < results.count else { return nil }
-        return DocumentSearchResult(pdfResult: results[index])
-    }
-
-    func search(for string: String) {
-        if string.isEmpty {
-            finishSearch(with: [])
-            delegate?.set(footer: nil)
-        }
-
-        let search = TextSearch(document: document)
-        search.delegate = self
-        search.comparisonOptions = [.caseInsensitive, .diacriticInsensitive]
-        search.search(for: string)
-        currentSearch = search
-    }
-
-    func select(index: Int) {
-        guard index < results.count else { return }
-        documentController.highlightSelectedSearchResult(results[index])
-    }
-
-    private func finishSearch(with results: [SearchResult]) {
-        delegate?.stopSearchLoadingIndicator()
-        self.results = results
-        documentController.highlightSearchResults(results)
-        delegate?.dataChanged()
-    }
-
-    func cancel() {
-        currentSearch?.cancelAllOperations()
-    }
-}
-
-extension PDFSearchHandler: TextSearchDelegate {
-    func willStart(_ textSearch: TextSearch, term searchTerm: String, isFullSearch: Bool) {
-        delegate?.startSearchLoadingIndicator()
-    }
-
-    func didFinish(_ textSearch: TextSearch, term searchTerm: String, searchResults: [SearchResult], isFullSearch: Bool, pageTextFound: Bool) {
-        finishSearch(with: searchResults)
-        if searchTerm.isEmpty {
-            delegate?.set(footer: nil)
-        } else {
-            delegate?.set(footer: L10n.Pdf.Search.matches(searchResults.count))
-        }
-    }
-
-    func didFail(_ textSearch: TextSearch, withError error: Error) {
-        finishSearch(with: [])
-        delegate?.set(footer: L10n.Pdf.Search.failed)
-    }
-
-    func didCancel(_ textSearch: TextSearch, term searchTerm: String, isFullSearch: Bool) {
-        delegate?.stopSearchLoadingIndicator()
-    }
 }
 
 final class DocumentSearchViewController: UIViewController {
@@ -207,7 +99,7 @@ final class DocumentSearchViewController: UIViewController {
             tableView.dataSource = self
             tableView.delegate = self
             tableView.keyboardDismissMode = UIDevice.current.userInterfaceIdiom == .pad ? .none : .onDrag
-            tableView.register(UINib(nibName: "PDFSearchCell", bundle: nil), forCellReuseIdentifier: DocumentSearchViewController.cellId)
+            tableView.register(UINib(nibName: "DocumentSearchCell", bundle: nil), forCellReuseIdentifier: DocumentSearchViewController.cellId)
             view.addSubview(tableView)
             self.tableView = tableView
 
@@ -279,7 +171,7 @@ extension DocumentSearchViewController: UITableViewDataSource, UITableViewDelega
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: DocumentSearchViewController.cellId, for: indexPath)
-        if let result = handler.result(at: indexPath.row), let cell = cell as? PDFSearchCell {
+        if let result = handler.result(at: indexPath.row), let cell = cell as? DocumentSearchCell {
             cell.setup(with: result)
         }
         return cell
