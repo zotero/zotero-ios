efc5cce112d784f91e724bee46337fe08f795bda
Miltiadis Vasilakis <mvasilak@gmail.com>
2025-08-30 07:18:46 +0300

    Improve LibrariesViewController code

--- Zotero/Scenes/Master/Libraries/Views/LibrariesViewController.swift
+++ Zotero/Scenes/Master/Libraries/Views/LibrariesViewController.swift
@@ -32,7 +32,7 @@ final class LibrariesViewController: UIViewController {
     init(viewModel: ViewModel<LibrariesActionHandler>, syncScheduler: SynchronizationScheduler) {
         self.viewModel = viewModel
         self.syncScheduler = syncScheduler
-        self.disposeBag = DisposeBag()
+        disposeBag = DisposeBag()
         super.init(nibName: "LibrariesViewController", bundle: nil)
     }
 
@@ -43,17 +43,36 @@ final class LibrariesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.setupNavigationBar()
-        self.setupTableView()
+        setupNavigationBar()
+        setupTableView()
+        viewModel.process(action: .loadData)
+        viewModel.stateObservable
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] state in
+                self?.update(to: state)
+            })
+            .disposed(by: disposeBag)
 
-        self.viewModel.process(action: .loadData)
+        func setupNavigationBar() {
+            let item = UIBarButtonItem(image: UIImage(systemName: "gear"))
+            item.tintColor = Asset.Colors.zoteroBlue.color
+            item.accessibilityLabel = L10n.Settings.title
+            item.rx.tap
+                .subscribe(onNext: { [weak self] _ in
+                    self?.coordinatorDelegate?.showSettings()
+                })
+                .disposed(by: disposeBag)
+            navigationItem.rightBarButtonItem = item
+        }
 
-        self.viewModel.stateObservable
-                      .observe(on: MainScheduler.instance)
-                      .subscribe(onNext: { [weak self] state in
-                          self?.update(to: state)
-                      })
-                      .disposed(by: self.disposeBag)
+        func setupTableView() {
+            tableView.dataSource = self
+            tableView.delegate = self
+            tableView.rowHeight = 44
+            tableView.separatorInset = UIEdgeInsets(top: 0, left: 60, bottom: 0, right: 0)
+            tableView.register(UINib(nibName: "LibraryCell", bundle: nil), forCellReuseIdentifier: Self.cellId)
+            tableView.tableFooterView = UIView()
+        }
     }
 
     override func viewIsAppearing(_ animated: Bool) {
@@ -71,19 +90,19 @@ final class LibrariesViewController: UIViewController {
 
     private func update(to state: LibrariesState) {
         if state.changes.contains(.groups) {
-            self.tableView.reloadData()
+            tableView.reloadData()
         }
 
         if state.changes.contains(.groupDeletion) {
-            self.showDefaultLibraryIfNeeded(for: state)
+            showDefaultLibraryIfNeeded(for: state)
         }
 
         if let error = state.error {
-            self.coordinatorDelegate?.show(error: error)
+            coordinatorDelegate?.show(error: error)
         }
 
         if let question = state.deleteGroupQuestion {
-            self.coordinatorDelegate?.showDeleteGroupQuestion(id: question.id, name: question.name, viewModel: self.viewModel)
+            coordinatorDelegate?.showDeleteGroupQuestion(id: question.id, name: question.name, viewModel: viewModel)
         }
     }
 
@@ -101,44 +120,21 @@ final class LibrariesViewController: UIViewController {
             }
         }
     }
-
-    // MARK: - Setups
-
-    private func setupTableView() {
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        self.tableView.rowHeight = 44
-        self.tableView.separatorInset = UIEdgeInsets(top: 0, left: 60, bottom: 0, right: 0)
-        self.tableView.register(UINib(nibName: "LibraryCell", bundle: nil), forCellReuseIdentifier: LibrariesViewController.cellId)
-        self.tableView.tableFooterView = UIView()
-    }
-
-    private func setupNavigationBar() {
-        let item = UIBarButtonItem(image: UIImage(systemName: "gear"))
-        item.tintColor = Asset.Colors.zoteroBlue.color
-        item.accessibilityLabel = L10n.Settings.title
-        item.rx.tap
-            .subscribe(onNext: { [weak self] _ in
-                self?.coordinatorDelegate?.showSettings()
-            })
-            .disposed(by: self.disposeBag)
-        self.navigationItem.rightBarButtonItem = item
-    }
 }
 
-extension LibrariesViewController: UITableViewDataSource, UITableViewDelegate {
+extension LibrariesViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        let groupCount = self.viewModel.state.groupLibraries?.count ?? 0
+        let groupCount = viewModel.state.groupLibraries?.count ?? 0
         return groupCount > 0 ? 2 : 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
-        case LibrariesViewController.customLibrariesSection:
-            return self.viewModel.state.customLibraries?.count ?? 0
+        case Self.customLibrariesSection:
+            return viewModel.state.customLibraries?.count ?? 0
 
-        case LibrariesViewController.groupLibrariesSection:
-            return self.viewModel.state.groupLibraries?.count ?? 0
+        case Self.groupLibrariesSection:
+            return viewModel.state.groupLibraries?.count ?? 0
 
         default:
             return 0
@@ -146,83 +142,79 @@ extension LibrariesViewController: UITableViewDataSource, UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return section == LibrariesViewController.groupLibrariesSection ? L10n.Libraries.groupLibraries : nil
+        return section == Self.groupLibrariesSection ? L10n.Libraries.groupLibraries : nil
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: LibrariesViewController.cellId, for: indexPath)
-
-        if let cell = cell as? LibraryCell,
-           let (name, state) = self.libraryData(for: indexPath) {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Self.cellId, for: indexPath)
+        if let cell = cell as? LibraryCell, let (name, state) = libraryData(for: indexPath) {
             cell.setup(with: name, libraryState: state)
         }
-
         return cell
-    }
 
+        func libraryData(for indexPath: IndexPath) -> (name: String, state: LibraryCell.LibraryState)? {
+            switch indexPath.section {
+            case Self.customLibrariesSection:
+                let library = viewModel.state.customLibraries?[indexPath.row]
+                return library.flatMap({ ($0.type.libraryName, .normal) })
+
+            case Self.groupLibrariesSection:
+                guard let library = viewModel.state.groupLibraries?[indexPath.row] else { return nil }
+                let state: LibraryCell.LibraryState
+                if library.isLocalOnly {
+                    state = .archived
+                } else if !library.canEditMetadata {
+                    state = .locked
+                } else {
+                    state = .normal
+                }
+                return (library.name, state)
+
+            default:
+                return nil
+            }
+        }
+    }
+}
+
+extension LibrariesViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        if let library = library(for: indexPath) {
+            coordinatorDelegate?.showCollections(for: library.identifier)
+        }
 
-        if let library = self.library(for: indexPath) {
-            self.coordinatorDelegate?.showCollections(for: library.identifier)
+        func library(for indexPath: IndexPath) -> Library? {
+            switch indexPath.section {
+            case Self.customLibrariesSection:
+                let library = viewModel.state.customLibraries?[indexPath.row]
+                return library.flatMap({ Library(customLibrary: $0) })
+
+            case Self.groupLibrariesSection:
+                let library = viewModel.state.groupLibraries?[indexPath.row]
+                return library.flatMap({ Library(group: $0) })
+
+            default:
+                return nil
+            }
         }
     }
 
     func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
-        guard indexPath.section == LibrariesViewController.groupLibrariesSection,
-              let group = self.viewModel.state.groupLibraries?[indexPath.row],
-              group.isLocalOnly else { return nil }
+        guard indexPath.section == Self.groupLibrariesSection, let group = viewModel.state.groupLibraries?[indexPath.row], group.isLocalOnly else { return nil }
 
         let groupId = group.identifier
         let groupName = group.name
 
         return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ -> UIMenu? in
-            return self.createContextMenu(for: groupId, groupName: groupName)
+            return createContextMenu(for: groupId, groupName: groupName)
         }
-    }
 
-    private func createContextMenu(for groupId: Int, groupName: String) -> UIMenu {
-        let delete = UIAction(title: L10n.remove, image: UIImage(systemName: "trash"), attributes: .destructive) { [weak self] _ in
-            self?.viewModel.process(action: .showDeleteGroupQuestion((groupId, groupName)))
-        }
-        return UIMenu(title: "", children: [delete])
-    }
-
-    private func libraryData(for indexPath: IndexPath) -> (name: String, state: LibraryCell.LibraryState)? {
-        switch indexPath.section {
-        case LibrariesViewController.customLibrariesSection:
-            let library = self.viewModel.state.customLibraries?[indexPath.row]
-            return library.flatMap({ ($0.type.libraryName, .normal) })
-
-        case LibrariesViewController.groupLibrariesSection:
-            guard let library = self.viewModel.state.groupLibraries?[indexPath.row] else { return nil }
-            let state: LibraryCell.LibraryState
-            if library.isLocalOnly {
-                state = .archived
-            } else if !library.canEditMetadata {
-                state = .locked
-            } else {
-                state = .normal
+        func createContextMenu(for groupId: Int, groupName: String) -> UIMenu {
+            let delete = UIAction(title: L10n.remove, image: UIImage(systemName: "trash"), attributes: .destructive) { [weak self] _ in
+                self?.viewModel.process(action: .showDeleteGroupQuestion((groupId, groupName)))
             }
-            return (library.name, state)
-
-        default:
-            return nil
-        }
-    }
-
-    private func library(for indexPath: IndexPath) -> Library? {
-        switch indexPath.section {
-        case LibrariesViewController.customLibrariesSection:
-            let library = self.viewModel.state.customLibraries?[indexPath.row]
-            return library.flatMap({ Library(customLibrary: $0) })
-
-        case LibrariesViewController.groupLibrariesSection:
-            let library = self.viewModel.state.groupLibraries?[indexPath.row]
-            return library.flatMap({ Library(group: $0) })
-
-        default:
-            return nil
+            return UIMenu(title: "", children: [delete])
         }
     }
 }
