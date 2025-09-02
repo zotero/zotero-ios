c8ffea9fea877d42a1676dae617aaca22e49d847
Miltiadis Vasilakis <mvasilak@gmail.com>
2025-09-02 14:18:13 +0300

    Improve Share extension ContainerViewController code
    Remove unnecessary code

--- ZShare/View Controllers/ContainerViewController.swift
+++ ZShare/View Controllers/ContainerViewController.swift
@@ -20,25 +20,25 @@ final class ContainerViewController: UIViewController {
         super.viewDidLoad()
 
         if UIDevice.current.userInterfaceIdiom == .pad {
-            self.setupForPad()
+            setupForPad()
         } else {
-            self.setupForPhone()
+            setupForPhone()
         }
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.didAppear = true
+        didAppear = true
     }
 
     override func preferredContentSizeDidChange(forChildContentContainer container: UIContentContainer) {
         super.preferredContentSizeDidChange(forChildContentContainer: container)
 
-        guard let height = self.containerHeight else { return }
+        guard let containerHeight else { return }
 
-        height.constant = container.preferredContentSize.height
+        containerHeight.constant = container.preferredContentSize.height
 
-        guard self.didAppear else { return }
+        guard didAppear else { return }
 
         UIView.animate(withDuration: 0.2) {
             self.view.layoutIfNeeded()
@@ -46,30 +46,30 @@ final class ContainerViewController: UIViewController {
     }
 
     private func setupForPad() {
-        self.view.backgroundColor = ProcessInfo().operatingSystemVersion.majorVersion == 13 ? UIColor.black.withAlphaComponent(0.085) : .clear
+        view.backgroundColor = .clear
 
-        self.containerView.layer.cornerRadius = 8
-        self.containerView.layer.masksToBounds = true
+        containerView.layer.cornerRadius = 8
+        containerView.layer.masksToBounds = true
 
-        let height = self.containerView.heightAnchor.constraint(equalToConstant: 100)
-        height.priority = .defaultHigh
-        self.containerHeight = height
+        let containerHeight = containerView.heightAnchor.constraint(equalToConstant: 100)
+        containerHeight.priority = .defaultHigh
+        self.containerHeight = containerHeight
 
         NSLayoutConstraint.activate([
-            self.view.centerXAnchor.constraint(equalTo: self.containerView.centerXAnchor),
-            self.view.centerYAnchor.constraint(equalTo: self.containerView.centerYAnchor),
-            self.containerView.heightAnchor.constraint(lessThanOrEqualTo: self.view.heightAnchor),
-            height,
-            self.containerView.widthAnchor.constraint(equalToConstant: ContainerViewController.padWidth)
+            view.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            view.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            containerView.heightAnchor.constraint(lessThanOrEqualTo: view.heightAnchor),
+            containerHeight,
+            containerView.widthAnchor.constraint(equalToConstant: Self.padWidth)
         ])
     }
 
     private func setupForPhone() {
         NSLayoutConstraint.activate([
-            self.containerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.containerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.containerView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            self.containerView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: view.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
