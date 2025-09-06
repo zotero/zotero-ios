fc0a07ae18d8661e6355215a7aa4c84a6a3ddbfd
Miltiadis Vasilakis <mvasilak@gmail.com>
2025-09-06 12:10:42 +0300

    Make CollectionCellContentView UI programmatic
    Remove unused CollectionCell.Accessories .chevronSpace option

--- Zotero/Scenes/Master/Collections/Views/CollectionCellContentView.swift
+++ Zotero/Scenes/Master/Collections/Views/CollectionCellContentView.swift
@@ -9,45 +9,120 @@
 import UIKit
 
 final class CollectionCellContentView: UIView {
-    @IBOutlet private weak var iconImage: UIImageView!
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var badgeContainer: UIView!
-    @IBOutlet private weak var badgeLabel: UILabel!
-    @IBOutlet private weak var chevronButton: UIButton!
-    @IBOutlet private weak var leftConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var separatorHeight: NSLayoutConstraint!
+    private weak var chevronButton: UIButton!
+    private weak var iconImageView: UIImageView!
+    private weak var iconImageViewLeadingConstraint: NSLayoutConstraint!
+    private weak var titleLabel: UILabel!
+    private weak var badgeContainer: UIView!
+    private weak var badgeLabel: UILabel!
     // These 2 need to be strong because they are being activated/deactivated
-    @IBOutlet private var contentToRightConstraint: NSLayoutConstraint!
-    @IBOutlet private var contentToBadgeConstraint: NSLayoutConstraint!
+    private var titleLabelTrailingConstraint: NSLayoutConstraint!
+    private var badgeContainerLeadingConstraint: NSLayoutConstraint!
 
     private var toggleCollapsedAction: (() -> Void)?
     private var chevronCollapsed: Bool = false
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        setupViews()
 
-        badgeContainer.layer.masksToBounds = true
-        badgeContainer.backgroundColor = badgeBackgroundColor
-        contentToRightConstraint.isActive = false
-        separatorHeight.constant = 1 / UIScreen.main.scale
-        var chevronConfig = UIButton.Configuration.plain()
-        chevronConfig.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
-        chevronButton.configuration = chevronConfig
-        chevronButton.configurationUpdateHandler = { [weak self] button in
-            let collapsed = self?.chevronCollapsed ?? false
-            var configuration = button.configuration
-            configuration?.image = UIImage(systemName: collapsed ? "chevron.right" : "chevron.down")?.applyingSymbolConfiguration(.init(scale: .small))
-            button.configuration = configuration
+        func setupViews() {
+            let chevronButton = UIButton(type: .custom, primaryAction: UIAction() { [weak self] _ in
+                self?.toggleCollapsedAction?()
+            })
+            var chevronButtonConfiguration = UIButton.Configuration.plain()
+            chevronButtonConfiguration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+            chevronButton.configuration = chevronButtonConfiguration
+            chevronButton.configurationUpdateHandler = { [weak self] button in
+                let collapsed = self?.chevronCollapsed ?? false
+                var configuration = button.configuration
+                configuration?.image = UIImage(systemName: collapsed ? "chevron.right" : "chevron.down")?.applyingSymbolConfiguration(.init(scale: .small))
+                button.configuration = configuration
+            }
+            chevronButton.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(chevronButton)
+            self.chevronButton = chevronButton
+
+            let iconImageView = UIImageView()
+            iconImageView.tintColor = Asset.Colors.zoteroBlue.color
+            iconImageView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(iconImageView)
+            self.iconImageView = iconImageView
+
+            let titleLabel = UILabel()
+            titleLabel.font = UIFont.preferredFont(forTextStyle: .body)
+            titleLabel.adjustsFontForContentSizeCategory = true
+            titleLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            titleLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(titleLabel)
+            self.titleLabel = titleLabel
+
+            let badgeContainer = UIView()
+            badgeContainer.backgroundColor = badgeBackgroundColor
+            badgeContainer.layer.masksToBounds = true
+            badgeContainer.setContentHuggingPriority(.required, for: .horizontal)
+            badgeContainer.setContentCompressionResistancePriority(.required, for: .horizontal)
+            badgeContainer.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(badgeContainer)
+            self.badgeContainer = badgeContainer
+
+            let badgeLabel = UILabel()
+            badgeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+            badgeLabel.textAlignment = .center
+            badgeLabel.setContentHuggingPriority(.required, for: .horizontal)
+            badgeLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+            badgeLabel.translatesAutoresizingMaskIntoConstraints = false
+            badgeContainer.addSubview(badgeLabel)
+            self.badgeLabel = badgeLabel
+
+            let separatorView = UIView()
+            separatorView.backgroundColor = UIColor.separator
+            separatorView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(separatorView)
+
+            let iconImageViewLeadingConstraint = iconImageView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: 32)
+            let titleLabelTrailingConstraint = safeAreaLayoutGuide.trailingAnchor.constraint(greaterThanOrEqualTo: titleLabel.trailingAnchor, constant: 16)
+            let badgeContainerLeadingConstraint = badgeContainer.leadingAnchor.constraint(greaterThanOrEqualTo: titleLabel.trailingAnchor, constant: 16)
+            self.iconImageViewLeadingConstraint = iconImageViewLeadingConstraint
+            self.titleLabelTrailingConstraint = titleLabelTrailingConstraint
+            self.badgeContainerLeadingConstraint = badgeContainerLeadingConstraint
+
+            NSLayoutConstraint.activate([
+                chevronButton.topAnchor.constraint(equalTo: topAnchor),
+                bottomAnchor.constraint(equalTo: chevronButton.bottomAnchor),
+                chevronButton.widthAnchor.constraint(equalToConstant: 48),
+                iconImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+                safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 8),
+                iconImageViewLeadingConstraint,
+                iconImageView.leadingAnchor.constraint(equalTo: chevronButton.trailingAnchor, constant: -4),
+                iconImageView.widthAnchor.constraint(equalToConstant: 28),
+                iconImageView.heightAnchor.constraint(equalToConstant: 28),
+                titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+                titleLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 16),
+                badgeContainer.centerYAnchor.constraint(equalTo: centerYAnchor),
+                layoutMarginsGuide.trailingAnchor.constraint(equalTo: badgeContainer.trailingAnchor),
+                badgeContainerLeadingConstraint,
+                badgeLabel.topAnchor.constraint(equalTo: badgeContainer.topAnchor, constant: 2),
+                badgeLabel.leadingAnchor.constraint(equalTo: badgeContainer.leadingAnchor, constant: 8),
+                badgeContainer.trailingAnchor.constraint(equalTo: badgeLabel.trailingAnchor, constant: 8),
+                badgeContainer.bottomAnchor.constraint(equalTo: badgeLabel.bottomAnchor, constant: 2),
+                separatorView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+                separatorView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                separatorView.bottomAnchor.constraint(equalTo: bottomAnchor),
+                separatorView.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale)
+            ])
         }
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         badgeContainer.layer.cornerRadius = badgeContainer.frame.height / 2.0
-    }
-
-    @IBAction private func toggleCollapsed() {
-        toggleCollapsedAction?()
     }
 
     private func set(collapsed: Bool) {
@@ -58,7 +133,7 @@ final class CollectionCellContentView: UIView {
 
     func set(collection: Collection, hasChildren: Bool, isCollapsed: Bool, accessories: CollectionCell.Accessories, toggleCollapsed: (() -> Void)?) {
         toggleCollapsedAction = toggleCollapsed
-        leftConstraint.constant = (accessories.contains(.chevron) || accessories.contains(.chevronSpace)) ? 32 : 8
+        iconImageViewLeadingConstraint.constant = accessories.contains(.chevron) ? 32 : 8
 
         setup(with: collection)
         updateBadgeView(for: accessories.contains(.badge) ? collection.itemCount : 0)
@@ -67,9 +142,9 @@ final class CollectionCellContentView: UIView {
 
     func set(libraryName: String, isCollapsed: Bool, accessories: CollectionCell.Accessories, toggleCollapsed: (() -> Void)?) {
         toggleCollapsedAction = toggleCollapsed
-        leftConstraint.constant = (accessories.contains(.chevron) || accessories.contains(.chevronSpace)) ? 32 : 8
+        iconImageViewLeadingConstraint.constant = accessories.contains(.chevron) ? 32 : 8
 
-        iconImage.image = Asset.Images.Cells.library.image.withRenderingMode(.alwaysTemplate)
+        iconImageView.image = Asset.Images.Cells.library.image.withRenderingMode(.alwaysTemplate)
         titleLabel.text = libraryName
 
         updateBadgeView(for: 0)
@@ -77,13 +152,13 @@ final class CollectionCellContentView: UIView {
     }
 
     func updateBadgeView(for itemCount: Int) {
-        badgeContainer.isHidden = itemCount == 0
+        badgeContainer.isHidden = (itemCount == 0)
         if !badgeContainer.isHidden {
             badgeLabel.text = "\(itemCount)"
             badgeLabel.accessibilityLabel = "\(itemCount) \(L10n.Accessibility.Collections.items)"
         }
-        contentToBadgeConstraint.isActive = !self.badgeContainer.isHidden || !self.chevronButton.isHidden
-        contentToRightConstraint.isActive = !self.contentToBadgeConstraint.isActive
+        badgeContainerLeadingConstraint.isActive = !badgeContainer.isHidden || !chevronButton.isHidden
+        titleLabelTrailingConstraint.isActive = !badgeContainerLeadingConstraint.isActive
     }
 
     private func setupChevron(visible: Bool, isCollapsed: Bool) {
@@ -94,14 +169,14 @@ final class CollectionCellContentView: UIView {
     }
 
     private func setup(with collection: Collection) {
-        iconImage.image = UIImage(named: collection.iconName)?.withRenderingMode(.alwaysTemplate)
+        iconImageView.image = UIImage(named: collection.iconName)?.withRenderingMode(.alwaysTemplate)
         titleLabel.text = collection.name
         titleLabel.accessibilityLabel = collection.name
     }
 
     private var badgeBackgroundColor: UIColor {
         return UIColor { traitCollection -> UIColor in
-            return UIColor.systemGray.withAlphaComponent(traitCollection.userInterfaceStyle == .dark ? 0.5 : 0.2)
+            return .systemGray.withAlphaComponent(traitCollection.userInterfaceStyle == .dark ? 0.5 : 0.2)
         }
     }
 }
