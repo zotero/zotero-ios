c4e5a0c292a38a50f0b52c6f48f810ce4efa7b8d
Michal Rentka <michalrentka@gmail.com>
2026-01-14 11:25:53 +0100

    WIP: Added load voices request

---
+++ Zotero/Controllers/API/Responses/VoicesResponse.swift
@@ -0,0 +1,14 @@
+//
+//  VoicesResponse.swift
+//  Zotero
+//
+//  Created by Michal Rentka on 14.01.2026.
+//  Copyright © 2026 Corporation for Digital Scholarship. All rights reserved.
+//
+
+struct VoiceResponse: Decodable {
+    let id: String
+    let label: String
+    let creditsPerSecond: Int
+    let segmentGranularity: String
+}
