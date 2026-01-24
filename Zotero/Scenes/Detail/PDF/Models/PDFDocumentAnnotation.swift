21bb0a4ef17a02e179512e5673865440dc5b38b9
Miltiadis Vasilakis <mvasilak@gmail.com>
2026-01-24 10:18:03 +0200

    Add custom PDF document annotation provider
    Add custom data for PDF annotation base color
    Improve PDF annotations reader key retrieval
    Use extra PDF document annotation colors in filtering
    Improve PDF annotations filtering
    Ignore PDF annotation links when filtering
    Convert PDF reader document annotations to lazy results
    Compute annotations pages index set in PDF reader action handler
    Use page sections in PDF annotations view controller
    Allow read annotations database request for specific page
    Cache document annotations based on file md5
    Make reader side bar load lazy
    Override class for locked document annotations
    Improve annotation preview deletion logic
    Improve code
    Fix typo

--- Zotero/Scenes/Detail/PDF/Models/PDFDocumentAnnotation.swift
+++ Zotero/Scenes/Detail/PDF/Models/PDFDocumentAnnotation.swift
@@ -61,3 +61,44 @@ extension PDFDocumentAnnotation: PDFAnnotation {
         return []
     }
 }
+
+extension PDFDocumentAnnotation {
+    init?(annotation: RDocumentAnnotation, isAuthor: Bool) {
+        // Some types may be cached because they are drawn, but not supported for further manipulation.
+        guard let annotationType = AnnotationType(rawValue: annotation.type) else { return nil }
+        let rects: [CGRect] = annotation.rects
+            .map({ CGRect(x: $0.minX, y: $0.minY, width: ($0.maxX - $0.minX), height: ($0.maxY - $0.minY)) })
+        let paths: [[CGPoint]] = annotation.paths.sorted(byKeyPath: "sortIndex")
+            .filter({ $0.coordinates.count % 2 == 0 })
+            .map({ path -> [CGPoint] in
+                let sortedCoordinates = path.coordinates.sorted(byKeyPath: "sortIndex")
+                return (0..<(path.coordinates.count / 2)).map({ index -> CGPoint in
+                    return CGPoint(x: sortedCoordinates[index * 2].value, y: sortedCoordinates[(index * 2) + 1].value).rounded(to: 3)
+                })
+            })
+        self.init(
+            key: annotation.key,
+            type: annotationType,
+            page: annotation.page,
+            pageLabel: annotation.pageLabel,
+            rects: rects,
+            paths: paths,
+            lineWidth: annotation.lineWidth.flatMap({ CGFloat($0) }),
+            author: annotation.author,
+            isAuthor: isAuthor,
+            color: annotation.color,
+            comment: annotation.comment,
+            text: annotation.text,
+            fontSize: annotation.fontSize.flatMap({ CGFloat($0) }),
+            rotation: annotation.rotation.flatMap({ UInt($0) }),
+            sortIndex: annotation.sortIndex,
+            dateAdded: annotation.dateAdded,
+            dateModified: annotation.dateModified
+        )
+    }
+
+    init?(annotation: RDocumentAnnotation, displayName: String, username: String) {
+        let isAuthor = annotation.author == displayName || annotation.author == username
+        self.init(annotation: annotation, isAuthor: isAuthor)
+    }
+}
