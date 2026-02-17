04635f51c4ca064265494ca47ff12118f22a1e50
Michal Rentka <michalrentka@gmail.com>
2026-02-17 14:28:34 +0100

    Highlight whole paragraph when speaking a sentence.

--- Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
@@ -1142,7 +1142,7 @@ extension PDFReaderViewController: SpeechManagerDelegate {
         documentController?.focus(page: pageIndex)
     }
 
-    func speechTextChanged(text: String, pageIndex: UInt) {
+    func highlightTextChanged(text: String, pageIndex: UInt) {
         documentController?.updateSpeechHighlight(text: text, page: PageIndex(pageIndex))
     }
 }
