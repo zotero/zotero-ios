8b165d0b51c583ab4f0fd8e4c5febaebe14ca383
Michal Rentka <michalrentka@gmail.com>
2026-03-05 14:43:17 +0100

    Speech voice picker improvements

--- Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
@@ -417,7 +417,7 @@ class PDFReaderViewController: UIViewController, ReaderViewController {
 
     private func showReadAloudOnboardingIfNeeded() {
         guard FeatureGates.enabled.contains(.speech),
-//              !Defaults.shared.didShowReadAloudOnboarding,
+              !Defaults.shared.didShowReadAloudOnboarding,
               !viewModel.state.document.isLocked,
               let speechManager = accessibilityHandler?.speechManager
         else { return }
