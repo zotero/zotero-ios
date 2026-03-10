86a0793a7d27ab6e197cbcff60a94d30bad77154
Michal Rentka <michalrentka@gmail.com>
2026-03-10 14:34:26 +0100

    Basic highlight overlay functionality

--- Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
@@ -1257,6 +1257,12 @@ extension PDFReaderViewController: SpeechManagerDelegate {
     func clearAnnotationPreview() {
         documentController?.clearAnnotationPreview()
     }
+
+    func createHighlightAnnotation(forSentence text: String, onPage pageIndex: UInt) {
+        let page = PageIndex(pageIndex)
+        guard let rects = documentController?.speechHighlightPDFFrames(for: text, page: page), !rects.isEmpty else { return }
+        viewModel.process(action: .createHighlight(pageIndex: page, rects: rects))
+    }
 }
 
 extension PDFReaderViewController: AccessibilityViewDelegate {
