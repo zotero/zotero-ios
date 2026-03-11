f5256f4a2b7f5afb3200cb840ef7b193cfa5d38b
Michal Rentka <michalrentka@gmail.com>
2026-03-11 15:20:58 +0100

    Refactoring, speech highlight navigation improvements, added tests

--- Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
@@ -1160,6 +1160,9 @@ extension PDFReaderViewController: SpeechManagerDelegate {
     }
 
     func highlightTextChanged(text: String, pageIndex: UInt) {
+        if documentController?.currentPage != pageIndex {
+            documentController?.focus(page: pageIndex)
+        }
         documentController?.updateSpeechHighlight(text: text, page: PageIndex(pageIndex))
     }
 
