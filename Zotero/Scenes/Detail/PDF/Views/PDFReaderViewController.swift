a6aa3e083929cb2804e6c5c09bde4bd2e9b1836e
Michal Rentka <michalrentka@gmail.com>
2026-03-19 15:26:53 +0100

    Forward/backward in speech manager should re-focus current page

--- Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
@@ -1187,6 +1187,10 @@ extension PDFReaderViewController: SpeechManagerDelegate {
         }
     }
 
+    func focusPage(_ pageIndex: UInt) {
+        documentController?.focus(page: pageIndex)
+    }
+
     func readAloudHighlightChanged(text: String, pageIndex: UInt) {
         documentController?.updateReadAloudHighlight(text: text, page: PageIndex(pageIndex))
     }
