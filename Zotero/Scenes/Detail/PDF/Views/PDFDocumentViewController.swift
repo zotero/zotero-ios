40b265de9846eebb84128cc5bd582fed64e922ef
Michal Rentka <michalrentka@gmail.com>
2026-04-10 14:21:52 +0200

    Fixed highlighting bug, skip "-" characters as well when mapping PDFWorker -> PSPDFKit parsed text

--- Zotero/Scenes/Detail/PDF/Views/PDFDocumentViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/PDFDocumentViewController.swift
@@ -1206,7 +1206,7 @@ extension PDFDocumentViewController: AnnotationBoundingBoxConverter {
                     characterCount += 1
                     glyphIndices.append(index)
 
-                    if !char.isWhitespace {
+                    if !char.isWhitespace && char != "-" {
                         glyphTextNoSpaces.append(char)
                         noSpaceToOriginalIndex.append(currentIndex)
                     }
@@ -1217,7 +1217,7 @@ extension PDFDocumentViewController: AnnotationBoundingBoxConverter {
             }
         }
 
-        let searchNoSpaces = searchText.filter { !$0.isWhitespace }
+        let searchNoSpaces = searchText.filter { !$0.isWhitespace && $0 != "-" }
         guard !searchNoSpaces.isEmpty else { return [] }
         guard let range = glyphTextNoSpaces.range(of: searchNoSpaces) else { return [] }
 
