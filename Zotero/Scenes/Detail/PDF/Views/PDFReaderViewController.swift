bac7ac9dafe4b47b1eb076c5b9030e7a46823147
Michal Rentka <michalrentka@gmail.com>
2026-05-14 14:56:02 +0200

    UI changes in voice pickers & renaming

--- Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
+++ Zotero/Scenes/Detail/PDF/Views/PDFReaderViewController.swift
@@ -1260,7 +1260,7 @@ extension PDFReaderViewController: ReadAloudViewDelegate {
         documentController?.clearAnnotationPreview()
     }
 
-    func showSpeechHighlighterOverlay(_ overlay: SpeechHighlighterOverlayView, isCompact: Bool, speechControlsView: UIView?, animated: Bool) {
+    func showSpeechHighlighterOverlay(_ overlay: ReadAloudHighlighterOverlayView, isCompact: Bool, speechControlsView: UIView?, animated: Bool) {
         view.addSubview(overlay)
         setupSpeechHighlighterOverlayConstraints(overlay, isCompact: isCompact, speechControlsView: speechControlsView)
         if !animated {
@@ -1273,7 +1273,7 @@ extension PDFReaderViewController: ReadAloudViewDelegate {
             }
         }
 
-        func setupSpeechHighlighterOverlayConstraints(_ overlay: SpeechHighlighterOverlayView, isCompact: Bool, speechControlsView: UIView?) {
+        func setupSpeechHighlighterOverlayConstraints(_ overlay: ReadAloudHighlighterOverlayView, isCompact: Bool, speechControlsView: UIView?) {
             if isCompact {
                 let bottomAnchor: NSLayoutYAxisAnchor
                 if let speechControlsView, speechControlsView.superview != nil {
@@ -1300,7 +1300,7 @@ extension PDFReaderViewController: ReadAloudViewDelegate {
         }
     }
 
-    func hideSpeechHighlighterOverlay(_ overlay: SpeechHighlighterOverlayView) {
+    func hideSpeechHighlighterOverlay(_ overlay: ReadAloudHighlighterOverlayView) {
         speechHighlighterTopConstraint = nil
         UIView.animate(withDuration: 0.15, delay: 0, options: .curveEaseIn, animations: {
             overlay.alpha = 0
