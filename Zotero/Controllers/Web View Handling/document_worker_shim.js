c653a2c5bdbf3463b973ac3208aba0d713a0c0a3
Miltiadis Vasilakis <mvasilak@gmail.com>
2026-06-12 12:26:19 +0300

    Update document worker submodule

--- Zotero/Controllers/Web View Handling/document_worker_shim.js
+++ Zotero/Controllers/Web View Handling/document_worker_shim.js
@@ -173,6 +173,58 @@
     };
   }
 
+  if (typeof g.TextEncoder === "undefined") {
+    g.TextEncoder = function () {
+      this.encoding = "utf-8";
+    };
+    g.TextEncoder.prototype.encode = function (input) {
+      var str = input === undefined ? "" : String(input);
+      var bytes = [];
+
+      for (var i = 0; i < str.length; i++) {
+        var code = str.charCodeAt(i);
+
+        if (code >= 0xd800 && code <= 0xdbff) {
+          var next = i + 1 < str.length ? str.charCodeAt(i + 1) : 0;
+          if (next >= 0xdc00 && next <= 0xdfff) {
+            code = 0x10000 + ((code - 0xd800) << 10) + (next - 0xdc00);
+            i++;
+          }
+          else {
+            code = 0xfffd;
+          }
+        }
+        else if (code >= 0xdc00 && code <= 0xdfff) {
+          code = 0xfffd;
+        }
+
+        if (code < 0x80) {
+          bytes.push(code);
+        }
+        else if (code < 0x800) {
+          bytes.push(0xc0 | (code >> 6), 0x80 | (code & 0x3f));
+        }
+        else if (code < 0x10000) {
+          bytes.push(
+            0xe0 | (code >> 12),
+            0x80 | ((code >> 6) & 0x3f),
+            0x80 | (code & 0x3f)
+          );
+        }
+        else {
+          bytes.push(
+            0xf0 | (code >> 18),
+            0x80 | ((code >> 12) & 0x3f),
+            0x80 | ((code >> 6) & 0x3f),
+            0x80 | (code & 0x3f)
+          );
+        }
+      }
+
+      return new Uint8Array(bytes);
+    };
+  }
+
   if (typeof g.AbortSignal === "undefined") {
     g.AbortSignal = function () {
       this.aborted = false;
